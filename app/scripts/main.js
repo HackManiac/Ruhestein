8f62f7d2d87407231412112f3a798ee6ab681156
Daniel Wippermann <Daniel.Wippermann@resol.de>
2013-09-03 09:35:16 +0200

    - Imported Yeoman generated project skeleton

---
+++ app/scripts/main.js
@@ -0,0 +1,54 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
+        bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
+        bootstrapButton: '../bower_components/sass-bootstrap/js/button',
+        bootstrapCarousel: '../bower_components/sass-bootstrap/js/carousel',
+        bootstrapCollapse: '../bower_components/sass-bootstrap/js/collapse',
+        bootstrapPopover: '../bower_components/sass-bootstrap/js/popover',
+        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/scrollspy',
+        bootstrapTab: '../bower_components/sass-bootstrap/js/tab',
+        bootstrapTooltip: '../bower_components/sass-bootstrap/js/tooltip',
+        bootstrapTransition: '../bower_components/sass-bootstrap/js/transition'
+    },
+    shim: {
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery']
+        },
+        bootstrapPopover: {
+            deps: ['jquery']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
