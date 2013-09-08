00e7f39d0bd83642e9725107d09efdade6414a63
Daniel Wippermann <Daniel.Wippermann@resol.de>
2013-09-08 22:06:01 +0200

    - Added Hunter's Mark effect

--- src/ruhestein/gamemaster/database/effects/hunters-mark-22.js
+++ src/ruhestein/gamemaster/database/effects/hunters-mark-22.js
@@ -14,9 +14,47 @@ var HuntersMark22 = {
         return this.formatDescription('Change a minion\'s Health to 1 this turn.');
     },
 
-    cast: function() {
-        throw new Error('No cast implementation for effect "HuntersMark22"');
+    targetLocations: 'allBattlefields',
+
+    cast: function(target) {
+        var buff = this.buffCard(target);
+
+        var didEndTurn = function() {
+            buff.uncast();
+        };
+
+        this.onEndOfNextPlayerTurn(didEndTurn);
     },
+
+    castBuff: function(target) {
+        this._target = target;
+        this._originalMaxHealth = target.getMaxHealth();
+        this._originalDamage = target.getDamage();
+
+        target.setDamage(0);
+        target.setMaxHealth(1);
+
+        this.listenToGame('didDealDamage', this._didDealDamage);
+    },
+
+    uncastBuff: function(target) {
+        if (target !== this._target) {
+            throw new Error('PANIC');
+        }
+
+        this.stopListeningToGame('didDealDamage', this._didDealDamage);
+
+        target.setMaxHealth(this._originalMaxHealth);
+        target.setDamage(this._originalDamage);
+
+        this._target = null;
+    },
+
+    _didDealDamage: function(info) {
+        if (info.card === this._target) {
+            this._target.healDamage(info.damage);
+        }
+    }
 
 };
 
--- test/spec/ruhestein-test/gamesimulation/effects/hunters-mark-22-test.js
+++ test/spec/ruhestein-test/gamesimulation/effects/hunters-mark-22-test.js
@@ -6,9 +6,6 @@
 
 
 
-var Ruhestein = require('ruhestein');
-
-
 var GameSimulationTestUtils = require('../gamesimulation-test-utils');
 
 
@@ -19,18 +16,25 @@ describe('HuntersMark22', function() {
 
     var setupDefaultGameTestEngine = GameSimulationTestUtils.setupDefaultGameTestEngine;
 
-    xit('should work correctly', function() {
+    it('should work "correctly"', function() {
         var g = setupDefaultGameTestEngine({
+            player1: {
+                deck: [
+                    '1 Bloodfen Raptor'
+                ],
+                playCards: 1
+            },
             player2: {
                 deck: [
+                    '1 Stormwind Knight',
                     '1 Hunter\'s Mark'
                 ],
+                playCards: 1
             }
         });
 
-        g.play(g.hand(0, 'Hunter\'s Mark'), 0, '{}');
-
-        // TODO
+        g.play(g.hand(0, 'Hunter\'s Mark'), g.oBattlefield(0, '3/2 Bloodfen Raptor'), '{Dead}', '3/1');
+        g.play(g.battlefield(0, '2/5 Stormwind Knight'), g.oBattlefield(0, '3/1 Bloodfen Raptor'), '2/2', '3/1');
     });
 
 });
