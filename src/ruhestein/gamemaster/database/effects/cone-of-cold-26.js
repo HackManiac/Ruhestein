b3f1ce94821d22b69a2ec81e746433fddc5064c9
Daniel Wippermann <Daniel.Wippermann@resol.de>
2013-09-09 20:57:14 +0200

    - Added Cone of Cold effect

--- src/ruhestein/gamemaster/database/effects/cone-of-cold-26.js
+++ src/ruhestein/gamemaster/database/effects/cone-of-cold-26.js
@@ -10,12 +10,40 @@
 
 var ConeOfCold26 = {
 
+    basePower: 1,
+    
     getDescription: function() {
         return this.formatDescription('Freeze a minion and the minions next to it, and deal 1 damage to them.');
     },
 
-    cast: function() {
-        throw new Error('No cast implementation for effect "ConeOfCold26"');
+    targetLocations: 'allBattlefields',
+
+    cast: function(target) {
+        var collection;
+        if (target.getOwner() === this.getPlayer()) {
+            collection = this.getPlayer().getBattlefield();
+        } else {
+            collection = this.getPlayer().getOpponentBattlefield();
+        }
+
+        var index = collection.indexOf(target);
+
+        var cards = [];
+        if (index > 0) {
+            cards.push(collection.at(index - 1));
+        }
+        cards.push(target);
+        if (index < (collection.length - 1)) {
+            cards.push(collection.at(index + 1));
+        }
+
+        var damage = this.getCurrentPower();
+
+        for (var i = 0; i < cards.length; i++) {
+            var card = cards [i];
+            this.dealDamage(damage, card);
+            card.setIsFrozen(true);
+        }
     },
 
 };
--- test/spec/ruhestein-test/gamesimulation/effects/cone-of-cold-26-test.js
+++ test/spec/ruhestein-test/gamesimulation/effects/cone-of-cold-26-test.js
@@ -6,9 +6,6 @@
 
 
 
-var Ruhestein = require('ruhestein');
-
-
 var GameSimulationTestUtils = require('../gamesimulation-test-utils');
 
 
@@ -19,18 +16,36 @@ describe('ConeOfCold26', function() {
 
     var setupDefaultGameTestEngine = GameSimulationTestUtils.setupDefaultGameTestEngine;
 
-    xit('should work correctly', function() {
+    it('should work correctly', function() {
         var g = setupDefaultGameTestEngine({
+            player1: {
+                deck: [
+                    '1 Wisp',
+                    '3 Malygos',
+                    '1 Wisp',
+                ],
+                playCards: 5
+            },
             player2: {
                 deck: [
-                    '1 Cone of Cold'
+                    '2 Cone of Cold'
                 ],
             }
         });
 
-        g.play(g.hand(0, 'Cone of Cold'), 0, '{}');
+        g.play(g.hand(0, 'Cone of Cold'), g.oBattlefield(2, '4/12 Malygos {ZZZ}'), '{Dead}', '4/11 {Frozen,ZZZ}');
+        g.oBattlefield(0, '1/1 Wisp {ZZZ}');
+        g.oBattlefield(1, '4/11 Malygos {Frozen,ZZZ}');
+        g.oBattlefield(3, '4/11 Malygos {Frozen,ZZZ}');
+        g.oBattlefield(4, '1/1 Wisp {ZZZ}');
 
-        // TODO
+        g.test.playSpellPowerPlus5();
+        g.play(g.hand(0, 'Cone of Cold'), g.oBattlefield(2, '4/11 Malygos {Frozen,ZZZ}'), '{Dead}', '4/5 {Frozen,ZZZ}');
+        g.oBattlefield(0, '1/1 Wisp {ZZZ}');
+        g.oBattlefield(1, '4/5 Malygos {Frozen,ZZZ}');
+        g.oBattlefield(3, '4/5 Malygos {Frozen,ZZZ}');
+        g.oBattlefield(4, '1/1 Wisp {ZZZ}');
+
     });
 
 });
