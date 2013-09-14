fd16d4e82fcdf7c58f2c613d025ad7ac931f8e8e
Daniel Wippermann <Daniel.Wippermann@resol.de>
2013-09-14 17:10:01 +0200

    - Added Holy Nova effect

--- src/ruhestein/gamemaster/database/effects/holy-nova-671.js
+++ src/ruhestein/gamemaster/database/effects/holy-nova-671.js
@@ -10,13 +10,25 @@
 
 var HolyNova671 = {
 
+    basePower: 2,
+
     getDescription: function() {
-        return this.formatDescription('Deal 2 damage to all enemies.  Restore #2 Health to all  friendly characters.');
+        return this.formatDescription('Deal {P} damage to all enemies.  Restore #2 Health to all  friendly characters.');
     },
 
     cast: function() {
-        throw new Error('No cast implementation for effect "HolyNova671"');
-    },
+        var damage = this.getCurrentPower();
+
+        var cards = this.collectCardsByLocation('opponentCharacters');
+        cards.forEach(function(card) {
+            this.dealDamage(damage, card);
+        }, this);
+
+        cards = this.collectCardsByLocation('characters');
+        cards.forEach(function(card) {
+            this.healDamage(2, card);
+        }, this);
+    }
 
 };
 
--- test/spec/ruhestein-test/gamesimulation/effects/holy-nova-671-test.js
+++ test/spec/ruhestein-test/gamesimulation/effects/holy-nova-671-test.js
@@ -6,9 +6,6 @@
 
 
 
-var Ruhestein = require('ruhestein');
-
-
 var GameSimulationTestUtils = require('../gamesimulation-test-utils');
 
 
@@ -19,18 +16,33 @@ describe('HolyNova671', function() {
 
     var setupDefaultGameTestEngine = GameSimulationTestUtils.setupDefaultGameTestEngine;
 
-    xit('should work correctly', function() {
+    it('should work correctly', function() {
         var g = setupDefaultGameTestEngine({
+            player1: {
+                deck: [
+                    '1 Malygos'
+                ],
+                playCards: 1
+            },
             player2: {
                 deck: [
+                    '1 Malygos',
                     '1 Holy Nova'
                 ],
+                playCards: 1
             }
         });
 
-        g.play(g.hand(0, 'Holy Nova'), 0, '{}');
+        g.test.dealDamage(10, g.hero('0/30'), '0/20');
+        g.test.dealDamage(10, g.battlefield(0, '4/12'), '4/2');
 
-        // TODO
+        g.oHero('0/30');
+        g.oBattlefield(0, '4/12');
+        g.play(g.hand(0, 'Holy Nova'), null, '{Dead}');
+        g.hero('0/22');
+        g.battlefield(0, '4/4');
+        g.oHero('0/23');
+        g.oBattlefield(0, '4/5');
     });
 
 });
